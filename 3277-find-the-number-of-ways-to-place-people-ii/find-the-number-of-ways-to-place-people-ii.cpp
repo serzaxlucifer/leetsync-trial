9517d57a74b132d2cc74631bc2d835757ca1074f
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2025-09-04 05:01:31 +0530

    Time: 130 ms (59.60%) | Memory: 41.1 MB (69.42%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            if (a[0] == b[0]) 
-                return a[1] > b[1];  
-            return a[0] < b[0];     
-        });
+        ranges::sort(points, {}, [](auto &p){ return pair(p[0], -p[1]);});
         int numPairs = 0;
 
         for (int i = 0; i < points.size(); i++) {
@@ -13,18 +9,14 @@ public:
             int aliceX = points[i][0];
             int aliceY = points[i][1];
             int insideY = -1e9 -1;
-            //cout << aliceX << "AliceY: " << aliceY << endl;
-            //cout << endl;
 
             for(int j = i + 1; j < points.size(); j++) {
                 int bobX = points[j][0];
                 int bobY = points[j][1];
-                //cout << bobX << "BobY: " << bobY << endl;
 
-                if (bobY > aliceY) continue;        //  breaks placement constraints
-                if (insideY >= bobY) continue;
-                //cout << "INCREMENTING" << endl;
+                if (bobY > aliceY || insideY >= bobY) continue;
                 numPairs++;
+
                 insideY = aliceY - bobY < aliceY - insideY ? bobY : insideY;
             }
         }
