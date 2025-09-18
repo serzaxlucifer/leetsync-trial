e56dbf67ae872ca74feb6adf00246bba35b75630
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2025-09-19 04:11:43 +0530

    Time: 180 ms (96.49%) | Memory: 347.5 MB (97.12%) - LeetSync

--- 3678-design-task-manager/design-task-manager.cpp
+++ 3678-design-task-manager/design-task-manager.cpp
@@ -1,39 +1,60 @@
 class TaskManager {
-  priority_queue<pair<int, int>> tasks;
-  unordered_map<int, int> taskPriority;
-  unordered_map<int, int> taskOwner;
-
 public:
-  TaskManager(vector<vector<int>>& tasks) {
-    for (const auto& task : tasks) {
-      add(task[0], task[1], task[2]);
+    unordered_map<int,pair<int,int>>taskList; 
+    //taskId -> {priority, userId}
+    priority_queue<pair<int,int>>pq; 
+    //{priority, taskId}
+    TaskManager(vector<vector<int>>& tasks) {
+        for(auto &vec : tasks){
+            int userId = vec[0];
+            int taskId = vec[1];
+            int priority = vec[2];
+            taskList[taskId] = {priority, userId};
+            pq.push({priority, taskId});
+        }
     }
-  }
     
-  void add(int userId, int taskId, int priority) {
-    tasks.push({priority, taskId});
-    taskPriority[taskId] = priority;
-    taskOwner[taskId] = userId;
-  }
-    
-  void edit(int taskId, int newPriority) {
-    tasks.push({newPriority, taskId});
-    taskPriority[taskId] = newPriority;
-  }
-    
-  void rmv(int taskId) {
-    taskPriority[taskId] = -1;
-  }
-    
-  int execTop() {
-    while (!tasks.empty()) {
-      const auto task = tasks.top();
-      tasks.pop();
-      if (task.first == taskPriority[task.second]) {
-        taskPriority[task.second] = -1;
-        return taskOwner[task.second];
-      }
+    void add(int userId, int taskId, int priority) {
+        taskList[taskId]={priority, userId};
+        pq.push({priority, taskId});
     }
-    return -1;
-  }
+    
+    void edit(int taskId, int newPriority) {
+        int currP = taskList[taskId].first;
+        int currU = taskList[taskId].second;
+        taskList[taskId] = {newPriority, currU};
+        pq.push({newPriority, taskId});
+    }
+    
+    void rmv(int taskId) {
+        taskList.erase(taskId);
+    }
+    
+    int execTop() {
+        //pq : {priority, taskId}
+        while(!pq.empty()){
+            auto top = pq.top();
+            int currP = top.first;
+            int currT = top.second;
+            pq.pop();
+            if (!taskList.count(currT)) continue; // task was removed
+            //check if this Priority is latest update one
+            int actualP = taskList[currT].first;
+            if(taskList.count(currT) && currP==actualP ){
+                int ret = taskList[currT].second;
+                taskList.erase(currT);
+                return ret;
+            }
+        }
+        return -1;
+    }
 };
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
