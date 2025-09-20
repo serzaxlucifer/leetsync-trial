d00cb2a5646cd403223e8c18351319a709f44a20
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2025-09-20 05:46:09 +0530

    Time: 72 ms (77.81%) | Memory: 194.8 MB (13.37%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.cpp
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -1,24 +1,59 @@
 class Spreadsheet {
 public:
-    unordered_map<string, int> mpp;
-    Spreadsheet(int rows) {}
-    void setCell(string cell, int value) { 
-        mpp[cell] = value; 
-    }
-    void resetCell(string cell) { 
-        mpp[cell] = 0; 
+    int getCell(string cell, int start, int end) {
+        bool mode = true;
+        int col = 0;
+        if (cell[start] >= '0' && cell[start] <= '9') {
+            mode = false;
+        }
+        if (mode) col = cell[start] - 'A';
+        int row = 0;
+        int i = mode ? start + 1: start;
+        for(; i <= end; i++) {
+            row = row*10 + (cell[i] - '0');
+        }
+        return mode ? sheet[row - 1][col] : row;
     }
 
-    int getValue(string s) {
-        s = s.substr(1);
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '+') {
-                string s1 = s.substr(0, i), s2 = s.substr(i + 1);
-                int left = s1[0] >= 'A' && s1[0] <= 'Z' ? mpp[s1] : stoi(s1);
-                int right = s2[0] >= 'A' && s2[0] <= 'Z' ? mpp[s2] : stoi(s2);
-                return left + right;
-            }
+    int sheet[1000]['Z' - 'A' + 1] = {0};
+
+    Spreadsheet(int rows) {
+    }
+    
+    void setCell(string cell, int value) {
+        int col = cell[0] - 'A';
+        int row = 0;
+        for(int i = 1; i < cell.length(); i++) {
+            row = row*10 + (cell[i] - '0');
         }
-        return 0;
+        sheet[row - 1][col] = value;
+    }
+    
+    void resetCell(string cell) {
+        setCell(cell, 0);
+    }
+    
+    int getValue(string formula) {
+        int length = formula.length();
+        int start = 1, end = 1;
+        int val1, val2;
+        for(int i = 1; i < length; i++)
+        {
+            if(formula[i] == '+') {
+                val1 = getCell(formula, start, end);
+                start = i + 1, end = i + 1;
+            }
+            end = i;
+        }
+        val2 = getCell(formula, start, end);
+        return val1 + val2;
     }
 };
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
