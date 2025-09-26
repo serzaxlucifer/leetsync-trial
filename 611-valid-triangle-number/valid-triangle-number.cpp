5f58a13a235299b998f69998392cefb7e2c58704
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2025-09-27 04:00:32 +0530

    Time: 27 ms (89.63%) | Memory: 16.7 MB (8.84%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -5,23 +5,12 @@ public:
         int answer = 0;
 
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                // Find the number foward just not greater than nums[j]
-                int left = j + 1;
-                int right = nums.size() - 1;
-                int validCount = 0;
-                
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    if (nums[i] + nums[j] > nums[mid]) {
-                        validCount = mid - j; // All elements from j+1 to mid are valid
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
+            int k = i + 2;
+            for (int j = i + 1; j < nums.size() && nums[i] != 0; j++) {
+                while(k < nums.size() && nums[j] + nums[i] > nums[k]) {
+                    k++;
                 }
-                
-                answer += validCount;
+                answer += (k - j - 1);
             }
         }
 
