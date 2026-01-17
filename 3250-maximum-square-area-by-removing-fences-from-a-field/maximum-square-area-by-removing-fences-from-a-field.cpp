e7fb72833ceffca6e642cb56b0a057aa7bcb26c6
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2026-01-18 03:04:14 +0530

    Time: 995 ms (52.75%) | Memory: 267.1 MB (54.33%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,33 +1,34 @@
 class Solution {
-    unordered_set<int> getEdges(vector<int>& fences, int border) {
-        unordered_set<int> st;
-        fences.push_back(1);
-        fences.push_back(border);
-        sort(fences.begin(), fences.end());
-        for (int i = 0; i < fences.size(); i++) {
-            for (int j = i + 1; j < fences.size(); j++) {
-                st.insert(fences[j] - fences[i]);
-            }
-        }
-        return st;
-    }
-
 public:
-    int maximizeSquareArea(int m, int n, vector<int>& hFences,
-                           vector<int>& vFences) {
-        auto hEdges = getEdges(hFences, m);
-        auto vEdges = getEdges(vFences, n);
-        int res = 0;
-        for (auto e : hEdges) {
-            if (vEdges.contains(e)) {
-                res = max(res, e);
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        hFences.push_back(1);
+        hFences.push_back(m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+
+        unordered_set<int> stt;
+        long long ans = 0;
+
+        for (int i = 0; i < hFences.size(); i++) {
+            for (int j = i + 1; j < hFences.size(); j++) {
+                stt.insert(abs(hFences[j] - hFences[i]));
             }
         }
-        if (res == 0) {
-            res = -1;
-        } else {
-            res = 1ll * res * res % 1000000007;
+
+        for (int i = 0; i < vFences.size(); i++) {
+            for (int j = i + 1; j < vFences.size(); j++) {
+                int val = abs(vFences[j] - vFences[i]);
+                if (stt.count(val)) {
+                    ans = max(ans, (long long)val);
+                }
+            }
         }
-        return res;
+
+        if (ans == 0) {
+            return -1;
+        }
+
+        long long MOD = 1e9 + 7;
+        return (ans * ans) % MOD;
     }
 };
