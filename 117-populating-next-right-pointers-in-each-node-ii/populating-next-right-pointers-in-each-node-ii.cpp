41d7901273b93548e58decd910a7acbcd77e1c15
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2026-01-18 19:50:41 +0530

    Time: 5 ms (89.58%) | Memory: 18.9 MB (46.42%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,50 +1,34 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
 
 class Solution {
 public:
     Node* connect(Node* root) {
         if (root == nullptr) {
-            return root;
+            return nullptr;
         }
-        
-        queue<Node*> levelOrder;
-        levelOrder.push(root);
-        Node* previous = nullptr;
 
-        while(!levelOrder.empty()) {
-            int frontierSize = levelOrder.size();
-            for (int i = 0; i < frontierSize; i++) {
-                Node* node = levelOrder.front();
-                levelOrder.pop();
-                node->next = previous;
+        Node* levelStart = root;
 
-                if (node->right != nullptr) {
-                    levelOrder.push(node->right);
+        while (levelStart != nullptr) {
+            Node* curr = levelStart;
+            Node* dummy = new Node(0); // Dummy node to represent the start of the next level
+            Node* prev = dummy;
+
+            while (curr != nullptr) {
+                if (curr->left != nullptr) {
+                    prev->next = curr->left;
+                    prev = prev->next;
                 }
 
-                if (node->left != nullptr) {
-                    levelOrder.push(node->left);
+                if (curr->right != nullptr) {
+                    prev->next = curr->right;
+                    prev = prev->next;
                 }
 
-                previous = node;
+                curr = curr->next;
             }
-            previous = nullptr;
+
+            levelStart = dummy->next; // Move to the next level
+            delete dummy; // Free the dummy node
         }
 
         return root;
