c1e13e98155d5d1b3f4e664a4683324bec0f7867
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2026-01-25 02:51:12 +0530

    Time: 121 ms (98.00%) | Memory: 107.7 MB (5.46%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,17 +1,40 @@
 class Solution {
-public:
-    int minPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int start = 0, end = nums.size() - 1;
-        int minimax = 0;
+private:
 
-        while(start < end) {
-            int pairSum = nums[start] + nums[end];
-            minimax = max(minimax, pairSum);
-            start++;
-            end--;
+    vector<int> countSort(vector<int>& arr) {
+        int n = arr.size();
+        int maxval = 0;
+        for (int i = 0; i < n; i++)
+            maxval = max(maxval, arr[i]);
+
+        vector<int> cntArr(maxval + 1, 0);
+
+        for (int i = 0; i < n; i++)
+            cntArr[arr[i]]++;
+
+        for (int i = 1; i <= maxval; i++)
+            cntArr[i] += cntArr[i - 1];
+
+        vector<int> ans(n);
+        for (int i = n - 1; i >= 0; i--) {
+            ans[cntArr[arr[i]] - 1] = arr[i];
+            cntArr[arr[i]]--;
         }
 
-        return minimax;
+        return ans;
+    }
+
+public:
+    int minPairSum(vector<int>& nums) {
+        nums = countSort(nums);
+
+        int mP = 0;
+        int i = 0;
+        while(i < nums.size() / 2) {
+            mP = max(mP, nums[i] + nums[nums.size() - i - 1]);
+            i ++;
+        }
+
+        return mP;
     }
 };
