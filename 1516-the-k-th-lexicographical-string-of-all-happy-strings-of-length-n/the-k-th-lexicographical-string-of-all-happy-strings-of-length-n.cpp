4a7a91524a7845c6d1b5b9a3408377b8fdca2305
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2026-03-14 16:01:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,65 +1,32 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        int numStrings = getTotalCombos(n);
-        if (numStrings < k) {
-            return "";
+
+        int total = 3 * (1 << (n-1));
+        if (k > total) return "";
+
+        k--; // make 0-indexed
+
+        string res;
+
+        // first character
+        int block = 1 << (n-1);
+        res += "abc"[k / block];
+        k %= block;
+
+        for (int i = 1; i < n; i++) {
+
+            block >>= 1;
+
+            string choices = "";
+            for(char c : "abc")
+                if(c != res.back())
+                    choices += c;
+
+            res += choices[k / block];
+            k %= block;
         }
-
-        string answer = "";
-        int perLetter = numStrings / 3;
-
-        if (k <= perLetter) {
-            answer += 'a';
-        } else if (k <= perLetter*2) {
-            answer += 'b';
-        } else {
-            answer += 'c';
-        }
-        k--;
-
-
-        answer += getChar(answer[0], n - 1, k % perLetter);
-        // now deal with n - 1 letters.
-
-        return answer;
-    }
-
-    string getChar(char prevLetter, int bound, int partition) { // 2 // 0
-        if (bound == 0) {
-            return "";
-        }
-
-        string res = "";
-
-        int possibilities = pow(2, bound); // 4
-        if (partition < possibilities / 2) {
-            res += getAppropriateChar(prevLetter, true);
-        } else {
-            res += getAppropriateChar(prevLetter, false);
-        }
-
-        res += getChar(res[0], bound - 1, partition % (possibilities / 2));
 
         return res;
-
-    }
-
-    int getTotalCombos(int n) {
-        if (n == 1) {
-            return 3;
-        } 
-        return 3 * pow(2, n - 1); 
-    }
-
-    char getAppropriateChar(char currentChar, bool mode) {
-        if (currentChar == 'a') {
-            return mode ? 'b' : 'c';
-        } else if (currentChar == 'b') {
-            return mode ? 'a' : 'c';
-        } else {
-            return mode ? 'a' : 'b';
-        }
-
     }
 };
