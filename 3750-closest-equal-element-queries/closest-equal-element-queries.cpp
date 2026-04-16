cd785d423ed4abc0e3a45a77b8969157723bade7
Mukul Malik <85161950+serzaxlucifer@users.noreply.github.com>
2026-04-17 04:02:30 +0530

    Time: 119 ms (99.50%) | Memory: 185.4 MB (95.48%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -1,44 +1,29 @@
 class Solution {
-public:
-    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        unordered_map<int, vector<int>> lookup;
-        vector<int> answer;
+ public:
+  vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+    const int n = nums.size();
+    vector<int> ans;
+    // minDist[i] := the minimum distance between nums[i], and any other index j
+    // in the circular array, where nums[j] == nums[i]
+    vector<int> minDist(n, n);
+    unordered_map<int, int> lastSeen;
 
-        for(int i = 0; i < nums.size(); i++) {
-            lookup[nums[i]].push_back(i);
-        }
-
-        for (int i = 0; i < queries.size(); i++) {
-            int element = nums[queries[i]];
-
-            if (lookup.find(element) == lookup.end() || lookup[element].size() == 1) {
-                answer.push_back(-1);
-                continue;
-            }
-
-            answer.push_back(findMinIndexDistanct(lookup[element], queries[i], nums.size() - 1));
-        }
-
-        return answer;
+    for (int i = 0; i < n * 2; ++i) {
+      const int index = i % n;
+      const int num = nums[index];
+      if (const auto it = lastSeen.find(num); it != lastSeen.cend()) {
+        const int prevIndex = it->second % n;
+        const int d = i - prevIndex;
+        minDist[index] = min(minDist[index], d);
+        minDist[prevIndex] = min(minDist[prevIndex], d);
+      }
+      lastSeen[num] = i;
     }
 
-    int findMinIndexDistanct(vector<int>& indices, int index, int size) {
-        int minVal = INT_MAX;
-        int lastIndex = indices[indices.size() - 1];
-        int indexInIndices =lower_bound(indices.begin(), indices.end(), index) -indices.begin();
-        int prevDistance = INT_MAX, nextDistance = INT_MAX;
+    for (const int query : queries)
+      ans.push_back(minDist[query] == n ? -1 : minDist[query]);
 
-        if (indexInIndices == 0) {
-            prevDistance = (size - lastIndex + 1) + indices[indexInIndices];
-            nextDistance = indices[indexInIndices + 1] - indices[indexInIndices];
-        } else if (indexInIndices == indices.size() - 1) {
-            prevDistance = indices[indexInIndices] - indices[indexInIndices - 1];
-            nextDistance = (size - indices[indexInIndices] + 1) + indices[0];
-        } else {
-            prevDistance = indices[indexInIndices] - indices[indexInIndices - 1];
-            nextDistance = indices[indexInIndices + 1] - indices[indexInIndices];
-        }
-
-        return min(prevDistance, nextDistance);
-    }
+    return ans;
+  }
 };
+
